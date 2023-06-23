c4b49060309a2f32db14ee902232e66888f2f475
Parkshouzhen <shouzhenn@gmail.com>
2023-06-23 15:13:08 +0900

    230623 Method&Event

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,25 @@
     <!--동적으로 클래스 부여하는 방법-->
     <h2 class="orange">원숭이는 {{ food }}를 좋아합니다.</h2>
     <h2 v-bind:class="{salmon: food==='orange', 'not-good': food==='rice'}">원숭이는 {{ food }}를 좋아합니다.</h2>
- </div>
+    <hr />
+    <!--조건부 렌더링, 반복문 v-if/v-show/v-for-->
+    <h2>당신의 나이는 {{ age }} 입니다.</h2>
+    <h3 v-if="age > 18">당신은 어른입니다.</h3>
+    <!--조건들 사이에 다른 태그는 들어갈 수 X-->
+    <h3 v-else-if="age > 13 && age <= 18">당신은 청소년입니다.</h3>
+    <h3 v-else>당신은 어린이 입니다.</h3>
+    <h2 v-if="display">(v-if)보입니다!!!</h2>
+    <h2 v-show="display">(v-show)보입니다!!!</h2>
+    <h2 v-for = "animal in animals" :key="animal">{{ animal }}</h2>
+    <h2 v-for = "(animal, index) in animals" :key="index">{{ animal }} 인덱스는=> {{ index }}</h2>
+    <ol>
+      <li v-for="(animal, index) in animals" :key="index">{{ animal }}</li>
+      <li v-for="(user, index) in users" :key="index">{{ user.name }}은 직업이 {{ user.job }}인 {{ user.gender }}이다.
+        <p v-for="skill in user.skill" :key="skill">{{ skill }}</p>
+      
+      </li>
+    </ol>
+</div>
 </template>
 
 <script>
@@ -27,13 +45,21 @@ export default {
   name: 'App',
   data(){
     return {
-      animal : "Monkey", //animal 변수를 template에서 연결 {{}}
+      //animal : "Monkey", //animal 변수를 template에서 연결 {{}}
       food : "banana", //단어가 바뀔 때마다 food로 연결된 모든 데이터가 업데이트 됨
       alertMsg : "<h2>경고입니다</h2>",
       //subscribeHTML : `<button onclick="document.querySelector('body').style.display='none'">구독하기</button>`
       imageSource : "https://placeimg.com/100/100/any",
       naverURL : "https://www.naver.com",
-      daumURL : "https://www.daum.net"
+      daumURL : "https://www.daum.net",
+      age : 10,
+      display : true,
+      animals : ["monkey", "dog", "cat", "panda"],
+      users : [
+        {name: 'scalper', job: 'developer', gender: 'male', skill:["html", "css", "javascript"]},
+        {name: 'john', job: 'designer', gender: 'male', skill:["html", "css", "javascript"]},
+        {name: 'su', job: 'student', gender: 'female', skill:["html", "css", "javascript"]}
+      ]
     };
   }
 }
---
+++ src/myMethod.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <h1>{{ count }}</h1>
+    <button v-on:click="addNumber(1)">증가</button>
+    <button v-on:click="minusNumber">감소</button>
+    <button v-on:mouseover="addNumber(10)">마우스오버10증가</button>
+    <h2>안녕</h2>
+</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      count : 0
+    };
+  },
+  methods: {
+    addNumber(value){
+      this.count = this.count + value;
+    },
+    minusNumber(){
+      console.log(this)
+      this.count -= 1;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button{
+  width: 200px;
+  height: 100px;
+  font-size: 24px;
+}
+
+
+
+</style>
